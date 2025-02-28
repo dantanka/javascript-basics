25462b507e77bd1d725cdc91c69b5c7285a538a4
dantanka <annakomendant2605@gmail.com>
2025-03-01 00:42:27 +0200

    Користувач вводить числа та вибирає операцію. Програма обчислює результат і виводить його. Виводить повідомлення, якщо введені некоректні дані.

---
+++ task_2.1.3/task_2.1.3.js
@@ -0,0 +1,39 @@
+let num1, num2, operation, result;
+
+do {
+    num1 = prompt("Введіть перше число:");
+    num2 = prompt("Введіть друге число:");
+    
+    if (isNaN(num1) || isNaN(num2) || num1.trim() === "" || num2.trim() === "") {
+        alert("Будь ласка, введіть коректні числа!");
+    } else {
+        num1 = Number(num1);
+        num2 = Number(num2);
+    }
+} while (isNaN(num1) || isNaN(num2));
+
+do {
+    operation = prompt("Введіть математичну операцію (+, -, *, /):");
+} while (!["+", "-", "*", "/"].includes(operation));
+
+switch (operation) {
+    case "+":
+        result = num1 + num2;
+        break;
+    case "-":
+        result = num1 - num2;
+        break;
+    case "*":
+        result = num1 * num2;
+        break;
+    case "/":
+        if (num2 === 0) {
+            alert("Ділення на нуль неможливе!");
+            result = "Помилка";
+        } else {
+            result = num1 / num2;
+        }
+        break;
+}
+
+alert(`Результат: ${result}`);
