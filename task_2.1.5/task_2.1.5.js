ebdfdb7cb5cd005aa27062ca8c1d75bdecb74d7d
dantanka <annakomendant2605@gmail.com>
2025-03-05 02:11:17 +0200

    - Користувач намагається вгадати число. - Програма дає підказки та веде до правильної відповіді.

--- task_2.1.5/task_2.1.5.js
+++ task_2.1.5/task_2.1.5.js
@@ -1,3 +1,20 @@
-let secretNumber;
-let usserNumber;
+let randomNumber;
+let userNumber;
+let attempts = 0;
+
+do {
+    userNumber = Number(prompt("Вгадайте число від 0 до 100:"));
+    attempts++;
+
+    if (isNaN(userNumber)) {
+        alert("Будь ласка, введіть число!");
+    } else if (userNumber < randomNumber) {
+        alert("Ваше число менше.");
+    } else if (userNumber > randomNumber) {
+        alert("Ваше число більше.");
+    } else {
+        alert(`Вітаємо, ви вгадали число ${userNumber} за ${attempts} спроб!`);
+    }
+}
+while (userNumber !== randomNumber);
 
